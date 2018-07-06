f9584e8c24803637ed6c929d409849528d656873
woduseh <woduseh@gmail.com>
2018-07-06 13:49:47 +0900

    initialize

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+    public Character Character;
+
+    public void ChangeGameScene()
+    {
+        SceneManager.LoadScene(Character.CurrentPin.SceneToLoad);
+    }
+}
